421f92eef7eb29b449228c97486801bcf19c8185
Shivraj Mohite <149065921+Shivraj0199@users.noreply.github.com>
2024-12-02 11:18:28 +0530

    app2.py

--- app2.py
+++ app2.py
@@ -1,14 +1,17 @@
 from flask import Flask
 import os
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_gee();
-        return '<h1>Hello9 from flask & Docker<h1>'
+def hello_gee():
+    return '<h1>Hello from Flask & Docker</h1>'
+
 @app.route('/hi')
-def hell();
-        return '<h1>Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii from Flask & Docker</h1>'
+def hello():
+    return '<h1>Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii from Flask & Docker</h1>'
 
 if __name__ == "__main__":
-        port = int(os.environ.get('PORT', 5000))
-        app.run(debug=True, host='0.0.0.0', port=port
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
